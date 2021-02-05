c17bf9aaee253c499b7a68f3be0aa89d6c2801c9
Dylan Crawshaw <dcrawsh@gmail.com>
2021-02-04 23:22:14 -0800

    add file structure

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('welcome to the server')
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.listen(port);
+
+console.log('Lyrics RESTful API server started on: ' + port);
